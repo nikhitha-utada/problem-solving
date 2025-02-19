bad33c7d187fc7245b8db70ee170f2800c4a1e14
nikhitha-utada <nikhithautada2003@gmail.com>
2025-02-19 09:29:18 +0530

    checking if a number is even or odd

--- conditionals_loops.py
+++ conditionals_loops.py
@@ -10,8 +10,9 @@ else:
     print("Enter valid input")
 
 # Determine if a number is even or odd
-num = int(input("Enter the number"))
+num = int(input("Enter the number: "))
 if(num%2==0):
     print(f"{num} is even")
 else:
     print(f"{num} is odd")
+

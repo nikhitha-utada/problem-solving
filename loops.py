0af2c1fc4dd0de2b0b000a797ad7fcaf2cc4fe87
nikhitha-utada <nikhithautada2003@gmail.com>
2025-02-27 18:47:53 +0530

    printing sum of even num from 1 to 50

--- loops.py
+++ loops.py
@@ -1,19 +1,30 @@
 # easy:
 # problem-1:
 #  Print all numbers from 1 to 100 using a  for  loop. 
-for i in range(1,101):
-    print(i)
+# for i in range(1,101):
+#     print(i)
 
-# problem-2:
-# Write a program to print the sum of the first  n  natural numbers. (n*n+1/ 2) 
-# way-1
-n = int(input("Enter the value of n: "))
-sum=0
-for i in range(1,n+1):
-    sum+=i
-print(f"sum of first {n} natural numbers is {sum}")
+# # problem-2:
+# # Write a program to print the sum of the first  n  natural numbers. (n*n+1/ 2) 
+# # way-1
+# n = int(input("Enter the value of n: "))
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(f"sum of first {n} natural numbers is {sum}")
 
-# way-2:
-n = int(input("Enter the value of n: "))
-sum = n*(n+1)//2
-print(sum)
+# # way-2:
+# n = int(input("Enter the value of n: "))
+# sum = n*(n+1)//2
+# print(sum)
+
+# problem-03:
+# all even numbers between 1 and 50 using a  while loop
+num=1
+while(num<51):
+    if num%2==0:
+        print(num)
+    num+=1 
+
+
+

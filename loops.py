e9f9586160712f16b7c4928a1d4ca6337fa1ca34
nikhitha-utada <nikhithautada2003@gmail.com>
2025-02-27 18:37:03 +0530

    printing sum of first n natural numbers way-1

--- loops.py
+++ loops.py
@@ -3,3 +3,12 @@
 #  Print all numbers from 1 to 100 using a  for  loop. 
 for i in range(1,101):
     print(i)
+
+# problem-2:
+# Write a program to print the sum of the first  n  natural numbers. (n*n+1/ 2) 
+# way-1
+n = int(input("Enter the value of n: "))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+print(f"sum of first {n} natural numbers is {sum}")
